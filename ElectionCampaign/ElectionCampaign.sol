bb127f571a634198c5ae0f50dc753fe18d1b7a8a
Umar Faruk <umar.faruk@practo.com>
2020-06-02 13:42:15 +0530

    update travel Expenses struct

--- ElectionCampaign/ElectionCampaign.sol
+++ ElectionCampaign/ElectionCampaign.sol
@@ -6,61 +6,230 @@ contract ElectionCampaign {
 		string bankName;
 		uint256 bankAmount;
 	}
-
 	struct OpeningBalance {
-        uint openingBalanceId;
-        string partyName;
 		uint256 cash;
 		uint256 otherDeposits;
 		OpeningBankBalance[] bankBalances;
 	}
-
-
-	// struct ElectionRecord {
-	// 	uint recordId;
-	// 	string partyName;
-	// 	OpeningBalance opBal;
-	// 	bool verifiedByECAgent;
+	struct GrossReceipt {
+		uint256 cash;
+		uint256 chequeAmount;
+	}
+	struct GrossExpenditure {
+		uint256 cash;
+		uint256 chequeAmount;
+		uint256 draft;
+	}
+	// struct StarCampaigner {
+	// 	string candidateName;
 	// }
-	mapping(uint => OpeningBalance) opBals;
+	struct TravelExpensesStarCampaigners {
+		uint trExpId;
+		string stateAndVenue;
+		string dateOfMeeting;
+		string[] starCampaigners;
+		string modeOfTravel;
+		string nameOfAircraftPayee;
+		uint256 totalExpenses;
+	}
+	struct ElectionRecord {
+		uint Id;
+		string partyName;
+		OpeningBalance opBal;
+		GrossReceipt grRec;
+		GrossExpenditure grExp;
+		TravelExpensesStarCampaigners[] trExpStCam;
+		bool verifiedByECAgent;
+	}
+	//TravelExpensesStarCampaigners[] trExpStCam;
+	mapping(uint => ElectionRecord ) records;
+	//mapping(uint =>  TravelExpensesStarCampaigners) trExps;
+	//mapping(uint => StarCampaigner) stCms;
+
     //OpeningBalance[] opBals;
 
-	function updateOpeningBankBalance(
-		uint _opBalId,
-		string memory _bName,
-        uint256 _bAmt
-	) public {
-		OpeningBankBalance memory b = OpeningBankBalance({
-			bankName: _bName,
-			bankAmount: _bAmt
-		});
-		opBals[_opBalId].bankBalances.push(b);
-	}
-
-	function addOpeningBalance(
-		uint _opBalId,
+	function addElectionRecord(
+		uint _id,
 		string memory _pName,
         uint256 _c,
         uint256 _oD,
 		string memory _bName,
         uint256 _bAmt
+		) public {
+			records[_id].Id = _id;
+			records[_id].partyName = _pName;
+			records[_id].opBal.cash = _c;
+			records[_id].opBal.otherDeposits = _oD;
+			OpeningBankBalance memory b = OpeningBankBalance({
+				bankName: _bName,
+				bankAmount: _bAmt
+			});
+			records[_id].opBal.bankBalances.push(b);
+			records[_id].verifiedByECAgent = false;
+	}
+
+	function getElectionRecord(uint _id) public view returns(
+		ElectionRecord memory str)
+    {
+		return records[_id];
+	}
+
+	function updateOpeningBankBalance(
+		uint _recordId,
+		string memory _bName,
+        uint256 _bAmt
 	) public {
-		opBals[_opBalId].openingBalanceId = _opBalId;
-		opBals[_opBalId].partyName = _pName;
-		opBals[_opBalId].cash = _c;
-		opBals[_opBalId].otherDeposits = _oD;
 		OpeningBankBalance memory b = OpeningBankBalance({
 			bankName: _bName,
 			bankAmount: _bAmt
 		});
-		opBals[_opBalId].bankBalances.push(b);
+		records[_recordId].opBal.bankBalances.push(b);
 	}
 
-	function getOpeningBalance(uint _id) public view returns(
-		OpeningBalance memory oB)
-    {
-		return opBals[_id];
+	function addGrossReceipt(
+		uint _recordId,
+		uint256 _c,
+		uint256 _chqAmt
+	) public {
+		records[_recordId].grRec.cash = _c;
+		records[_recordId].grRec.chequeAmount = _chqAmt;
 	}
+
+	function addGrossExpenditure(
+		uint _recordId,
+		uint256 _c,
+		uint256 _chqAmt,
+		uint256 _draft
+	) public {
+		records[_recordId].grExp.cash = _c;
+		records[_recordId].grExp.chequeAmount = _chqAmt;
+		records[_recordId].grExp.draft = _draft;
+	}
+
+	string[] starC;
+	function addTravelExpensesStarCampaigners(
+		uint _recordId,
+		uint _trExpId,
+		string memory _stVen,
+		string memory _dOfMeet,
+		string memory _starCampaigner1,
+		string memory _mOfTr,
+		string memory _nOfPayee
+	) public {
+		// StarCampaigner memory sc = StarCampaigner({
+		// 	candidateName: _starCampaigner1
+		// });
+		// TravelExpensesStarCampaigners memory trExp = TravelExpensesStarCampaigners({
+		// 	trExpId: _trExpId,
+		// 	stateAndVenue: _stVen,
+		// 	dateOfMeeting: _dOfMeet,
+		// 	starCampaigners: sc,
+		// 	modeOfTravel: _mOfTr,
+		// 	nameOfAircraftPayee: _nOfPayee,
+		// 	totalExpenses: 0
+		// });
+		//string[] storage starC;
+		//records[_recordId].trExpStCam[_trExpId].starCampaigners;
+		starC.push(_starCampaigner1);
+		TravelExpensesStarCampaigners memory trExp = TravelExpensesStarCampaigners(
+			_trExpId,
+			_stVen,
+			_dOfMeet,
+			starC,
+			_mOfTr,
+			_nOfPayee,
+			0
+		);
+		//starC.clear();
+		delete starC;
+		records[_recordId].trExpStCam.push(trExp);
+
+		// trExps[_trExpId] = TravelExpensesStarCampaigners(
+		// 	_trExpId,
+		// 	_stVen,
+		// 	_dOfMeet,
+
+		// 	_mOfTr,
+		// 	_nOfPayee,
+		// 	0
+		// );
+		// stCms[] = StarCampaigner(_starCampaigner1);
+
+		// if (records[_recordId].trExpStCam.length == 0){
+		// 	return ;
+		// }
+		// TravelExpensesStarCampaigners storage tr = records[_recordId].trExpStCam;
+		// //TravelExpensesStarCampaigners storage
+		// //tr = records[_recordId].trExpStCam;
+		// tr[_trExpId].trExpId = _trExpId;
+		// tr[_trExpId].stateAndVenue = _stVen;
+		// tr[_trExpId].dateOfMeeting = _dOfMeet;
+		// tr[_trExpId].starCampaigners.push(_starCampaigner1);
+		// tr[_trExpId].modeOfTravel = _mOfTr;
+		// tr[_trExpId].nameOfAircraftPayee = _nOfPayee;
+		// tr[_trExpId].totalExpenses = 0;
+
+		// records[_recordId].trExpStCam.push(tr);
+		// //records[_recordId].trExpStCam[]
+
+		if (records[_recordId].trExpStCam.length == 0){
+			return ;
+		}
+
+		//trExps[_trExpId].trExpId = _trExpId;
+		// records[_recordId].trExpStCam[_trExpId].trExpId = _trExpId;
+		// records[_recordId].trExpStCam[_trExpId].stateAndVenue = _stVen;
+		// records[_recordId].trExpStCam[_trExpId].dateOfMeeting = _dOfMeet;
+		// records[_recordId].trExpStCam[_trExpId].modeOfTravel = _mOfTr;
+		// records[_recordId].trExpStCam[_trExpId].nameOfAircraftPayee = _nOfPayee;
+		// records[_recordId].trExpStCam[_trExpId].totalExpenses = 0;
+		 //records[_recordId].trExpStCam[_trExpId].starCampaigners.push(sc);
+	}
+
+	function addStarCampaignerInRecord(
+		uint _recordId,
+		uint _trExpId,
+		string _starCampaignerName
+	) public returns (uint) {
+		// StarCampaigner memory sc = StarCampaigner({
+		// 	candidateName: _starCampaignerName
+		// });
+		//records[_recordId].trExpStCam[_trExpId].starCampaigners.push(sc);
+
+		TravelExpensesStarCampaigners storage trE = records[_recordId].trExpStCam[_trExpId-1];
+		trE.starCampaigners.push(_starCampaignerName);
+
+		return trE.starCampaigners.length;
+	}
+
+
+
+	// function addOpeningBalance(
+	// 	uint _opBalId,
+	// 	string memory _pName,
+    //     uint256 _c,
+    //     uint256 _oD,
+	// 	string memory _bName,
+    //     uint256 _bAmt
+	// ) public {
+	// 	opBals[_opBalId].openingBalanceId = _opBalId;
+	// 	opBals[_opBalId].partyName = _pName;
+	// 	opBals[_opBalId].cash = _c;
+	// 	opBals[_opBalId].otherDeposits = _oD;
+	// 	OpeningBankBalance memory b = OpeningBankBalance({
+	// 		bankName: _bName,
+	// 		bankAmount: _bAmt
+	// 	});
+	// 	opBals[_opBalId].bankBalances.push(b);
+	// }
+
+	// function getOpeningBalance(uint _id) public view returns(
+	// 	OpeningBalance memory oB)
+    // {
+	// 	return opBals[_id];
+	// }
+
+
 
 
 
--- myApp.py
+++ myApp.py
@@ -1,6 +1,6 @@
 import json
 from web3 import Web3
-from deployContract import contract_abi, contract_address, ganache_url
+from deployContract import contract_abi, contract_address, ganache_url, senderAccount
 
 web3 = Web3(Web3.HTTPProvider(ganache_url))
 
@@ -20,49 +20,43 @@ contract = web3.eth.contract(address= contract_address , abi=contract_abi)
 # print(tx_hash)
 # print("got")
 # print(contract.functions.getOpeningBalance(0).call())
-print("stored")
-tx_hash = contract.functions.addOpeningBalance(1,'p1',2121,32424,'B1',34234).transact()
-print(tx_hash)
-print("stored")
-tx_hash = contract.functions.updateOpeningBankBalance(1,'B2',32224234).transact()
-print(tx_hash)
+tx_hash = contract.functions.addElectionRecord(1,'p1',2121,32424,'B1',34234).transact()
+print(web3.toHex(tx_hash))
+print("rec")
+print(contract.functions.getElectionRecord(1).call())
+print("updated")
+tx_hash = contract.functions.updateOpeningBankBalance(1,'B2',342232334).transact()
+print(web3.toHex(tx_hash))
+# print("got")
+# print(contract.functions.getElectionRecord(1).call())
+# print("gross")
+# tx_hash = contract.functions.addGrossReceipt(1,123132123,9992334).transact()
+# print("got")
+# print(contract.functions.getElectionRecord(1).call())
+# print("gross exp")
+# tx_hash = contract.functions.addGrossExpenditure(1,123132123,9992334,2211).transact()
 print("got")
-print(contract.functions.getOpeningBalance(1).call())
-print("stored")
-tx_hash = contract.functions.addOpeningBalance(2,'p1',2122221,32424,'B1',3499234).transact()
-print(tx_hash)
-print("stored")
-tx_hash = contract.functions.updateOpeningBankBalance(2,'B2',322212224234).transact()
-print(tx_hash)
+print(contract.functions.getElectionRecord(1).call())
+print("travelExp")
+# transaction = contract.functions.addTravelExpensesStarCampaigners(1,1,'State1','2020-04-21','cand1','aircr','payee1').buildTransaction()
+# transaction['nonce'] = web3.eth.getTransactionCount(senderAccount.address)
+# transaction['gas'] = 2000000
+# transaction['gasPrice'] = web3.toWei('50', 'gwei')
+# signed = web3.eth.account.signTransaction(transaction, senderAccount.privateKey)
+# tx_hash = web3.eth.sendRawTransaction(signed.rawTransaction)
+tx_hash = contract.functions.addTravelExpensesStarCampaigners(1,1,'State1','2020-04-21','cand1','aircr','payee1').transact()
+print(web3.toHex(tx_hash))
 print("got")
-print(contract.functions.getOpeningBalance(2).call())
+print(contract.functions.getElectionRecord(1).call())
+print("update star Campaigner")
+tx_hash = contract.functions.addStarCampaignerInRecord(1,1,'Cand2').transact()
+print(web3.toHex(tx_hash))
+print("got")
+print(contract.functions.getElectionRecord(1).call())
 
 
 
 
-##new Book
-# print("stored")
-# tx_hash = contract.functions.addChapter(1,'p1',34234).transact()
-# print(tx_hash)
-# print("stored")
-# tx_hash = contract.functions.addChapter(1,'t2',34443224234).transact()
-# print(tx_hash)
-# print("got")
-# print(contract.functions.getChapterByBook(1).call())
 
 
-
-##Storage
-# print("stored")
-# tx_hash = contract.functions.store(13787875875655).transact()
-# print(tx_hash)
-# print("got")
-# print(contract.functions.retreive().call())
-
-##Courses
-# print(contract.functions.countInstructors().call())
-# print("stored")
-# print(contract.functions.setInstructor('0x824A7944bF287F02F9EbAE68020181735edAE1CD',23,'F',"L").transact())
-# print("got")
-# print(contract.functions.countInstructors().call())
 
